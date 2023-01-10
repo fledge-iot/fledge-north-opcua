bb47648f1f481833f31bfac35876bbc58e1c200d
ashish-jabble <ashish@dianomic.com>
2023-01-10 21:46:42 +0530

    freeopcua checkout branch corrected
    Signed-off-by: ashish-jabble <ashish@dianomic.com>

--- requirements.sh
+++ requirements.sh
@@ -38,41 +38,41 @@ if [[ ( $os_name == *"Red Hat"* || $os_name == *"CentOS"* ) ]]; then
         export CXX=/opt/rh/devtoolset-7/root/usr/bin/g++
     fi
 elif apt --version 2>/dev/null; then
-	echo Installing boost components
-	sudo apt install -y libboost-filesystem-dev
-	sudo apt install -y libboost-program-options-dev
+    echo Installing boost components
+    sudo apt install -y libboost-filesystem-dev
+    sudo apt install -y libboost-program-options-dev
 else
-	echo "Requirements cannot be automatically installed, please refer README.rst to install requirements manually"
+    echo "Requirements cannot be automatically installed, please refer README.rst to install requirements manually"
 fi
 
-if [ $# -eq 1 ]; then
-	directory=$1
-	if [ ! -d $directory ]; then
-		mkdir -p $directory
-	fi
+if [[ $# -eq 1 ]]; then
+    directory=$1
+    if [[ ! -d $directory ]]; then
+        mkdir -p $directory
+    fi
 else
-	directory=~
+    directory=~
 fi
 
-if [ ! -d $directory/freeopcua ]; then
-	cd $directory
-	echo Fetching Free OPCUA library
-	git clone https://github.com/dianomic/freeopcua.git
-	git checkout Kapsch
-	cd freeopcua
-	mkdir build
-	sed -e 's/option(SSL_SUPPORT_MBEDTLS "Support rsa-oaep password encryption using mbedtls library " ON)/option(SSL_SUPPORT_MBEDTLS "Support rsa-oaep password encryption using mbedtls library " OFF)/' \
-		-e 's/add_library(opcuaclient/add_library(opcuaclient STATIC/' \
-		-e 's/add_library(opcuacore/add_library(opcuacore STATIC/' \
-		-e 's/add_library(opcuaprotocol/add_library(opcuaprotocol STATIC/' \
-		-e 's/add_library(opcuaserver/add_library(opcuaserver STATIC/' \
-		< CMakeLists.txt > CMakeLists.txt.$$ && mv CMakeLists.txt CMakeLists.txt.orig && \
-		mv CMakeLists.txt.$$ CMakeLists.txt
-	cd build
+if [[ ! -d $directory/freeopcua ]]; then
+    cd $directory
+    echo Fetching Free OPCUA library
+    git clone https://github.com/dianomic/freeopcua.git
+    cd freeopcua
+    git checkout Kapsch
+    mkdir build
+    sed -e 's/option(SSL_SUPPORT_MBEDTLS "Support rsa-oaep password encryption using mbedtls library " ON)/option(SSL_SUPPORT_MBEDTLS "Support rsa-oaep password encryption using mbedtls library " OFF)/' \
+        -e 's/add_library(opcuaclient/add_library(opcuaclient STATIC/' \
+        -e 's/add_library(opcuacore/add_library(opcuacore STATIC/' \
+        -e 's/add_library(opcuaprotocol/add_library(opcuaprotocol STATIC/' \
+        -e 's/add_library(opcuaserver/add_library(opcuaserver STATIC/' \
+        < CMakeLists.txt > CMakeLists.txt.$$ && mv CMakeLists.txt CMakeLists.txt.orig && \
+        mv CMakeLists.txt.$$ CMakeLists.txt
+    cd build
 
-	cmake ..
-	make
-	cd ..
-	echo Set the environment variable FREEOPCUA to $(pwd)
-	echo export FREEOPCUA=$(pwd)
+    cmake ..
+    make
+    cd ..
+    echo Set the environment variable FREEOPCUA to $(pwd)
+    echo export FREEOPCUA=$(pwd)
 fi
